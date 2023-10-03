e896ef28bfb072dd91c31e3f71f9b5f22aecc76f
emankh1 <ealkhat1@my.centennialcollege.ca>
2023-10-02 23:37:28 -0400

    adding files

---
+++ Eman_Lab1/public/js/form.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+    $('.form').on('submit', function (event) {
+        event.preventDefault();
+        this.reset();
+        console.log("submitted form ");
+
+    });
+});
