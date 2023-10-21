09d0c6049e7e17e6a9050ec6b10842fcba0ff501
emankh1 <144483893+emankh1@users.noreply.github.com>
2023-10-21 01:38:56 -0400

    Delete Eman_Lab1 directory

--- Eman_Lab1/public/js/form.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-
-    $('.form').on('submit', function (event) {
-        event.preventDefault();
-        this.reset();
-        console.log("submitted form ");
-
-    });
-});
